3de336b02b13a98bf016bf3ced7b9aafabceecbc
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2026-01-02 19:29:08 +0800

    Added missing fields

--- state.py
+++ state.py
@@ -1,17 +1,21 @@
-from typing import Annotated, List
-from operator import add
+from typing import List, Dict, Any
 from pydantic import BaseModel, Field
 
 class GraphState(BaseModel):
     job_description: str
+    is_valid_jd: bool = False
     cleaned_jd: str = ""
-    retrieved_chunks: Annotated[List[str], add] = Field(default_factory=list)
+    retrieved_chunks: List[Dict[str, Any]] = Field(default_factory=list)
     candidate_summary: str = ""
     resume_summary: str = ""
-    cover_letter: str = "   "
+    cover_letter: str = ""
     critique_feedback: str = ""
-    relevance_score: int = 0
+    relevance_score: float = 0
     needs_rewrite: bool = False
     grading_feedback: str = ""
     needs_refinement: bool = False
     refinement_count: int = 0
+    error_type: str = ""            
+    error_message: str = ""         
+    is_fallback: bool = False       
+    final_response: str = ""
